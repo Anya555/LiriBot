5c7f59d4a59340d971aeb9cc1f0d87b19d4a8b1e
Anna Panas <annapanas0906@gmail.com>
2020-01-24 10:05:03 -0500

    removed inquirer, restructured code

--- liri.js
+++ liri.js
@@ -4,18 +4,19 @@ const axios = require("axios");
 var inquirer = require("inquirer");
 var fs = require('fs');
 
-// var spotify = new Spotify(keys.spotify);
-// var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
 
+var command = process.argv[2];  // concert-this, spotify-this-song
+var extrainfo = process.argv[3];  // name of artist, name of song
 
 // console.log(keys.spotify.id);
 // console.log(keys.spotify.secret);
 
-var command = process.argv[2];
+// var command = parseInt(process.argv[2]);
+// var artistChoice = process.argv[3];
 
-// runCommand();
-
-// function runCommand(){
+// 
 // switch(command) {
 //     case 'concert-this':
 //       artist();
@@ -32,7 +33,38 @@ var command = process.argv[2];
 //     default:
      
 //   }
-// }
+// 
+    var song = function(){
+        // extraInfo is the song name
+        spotify.request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+        .then(function(data) {
+            console.log(data); 
+
+
+            
+        })
+        .catch(function(err) {
+            console.error('Error occurred: ' + err); 
+        });
+    }
+
+// function artist(){
+    // inquirer.prompt([
+    //     {
+    //         type: "input",
+    //         name: "name",
+    //         message: "Enter a name of an artist you're looking for:"
+    //     }
+    // ]).then(function () {
+    //         const queryUrl = `https://rest.bandsintown.com/artists/ = artistChoice = events?app_id=codingbootcamp`;
+    //         axios.get(queryUrl).then(function (res) {
+    //             console.log(res.data);
+                // console.log(res.venue.name);
+                // console.log(res.venue.city);
+                // console.log(res.datetime);
+        //     });
+        // });
+    // }
 
 // function movie(){
 // inquirer.prompt([
@@ -45,24 +77,19 @@ var command = process.argv[2];
 //         const queryUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=38168999`;
 //         axios.get(queryUrl).then(function (res) {
 //             console.log(res.data);
-//         });
-//     });
+            // console.log(res.Title);
+            // console.log(res.Year);
+            // console.log(res.imdbRating);
+            // console.log(res.Ratings[1]);
+            // console.log(res.Country);
+            // console.log(res.Language);
+            // console.log(res.Plot);
+            // console.log(res.Actors);
+    //     });
+    // });
 // }
 
-// function artist(){
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter a name of an artist you're looking for:"
-        }
-    ]).then(function (name) {
-            const queryUrl = `https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp`;
-            axios.get(queryUrl).then(function (res) {
-                console.log(res.data);
-            });
-        });
-    // }
+// 
+// 
 
-
-   
+ 
