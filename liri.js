d3d84cd1412d04f99f743c2b500ec1d467fe702b
Anna Panas <annapanas0906@gmail.com>
2020-01-23 22:40:40 -0500

    added bandsintown API call function. Getting response back

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var keys = require("./keys.js");
 const axios = require("axios");
 var inquirer = require("inquirer");
 var fs = require('fs');
+
 // var spotify = new Spotify(keys.spotify);
 // var Spotify = require('node-spotify-api');
 
@@ -10,22 +11,58 @@ var fs = require('fs');
 // console.log(keys.spotify.id);
 // console.log(keys.spotify.secret);
 
-inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "Enter a name of the movie you're looking for:"
-    }
-]).then(function (name) {
-        const queryUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=38168999`;
-        axios.get(queryUrl).then(function (res) {
-            console.log(res.data);
+var command = process.argv[2];
+
+// runCommand();
+
+// function runCommand(){
+// switch(command) {
+//     case 'concert-this':
+//       artist();
+//       break;
+//     case 'spotify-this-song':
+        //  song();
+//       break;
+//     case 'movie-this':
+        // movie();
+//       break;
+//     case 'do-what-it-says':
+        //  doThis();
+//       break;
+//     default:
+     
+//   }
+// }
+
+// function movie(){
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "Enter a name of the movie you're looking for:"
+//     }
+// ]).then(function (name) {
+//         const queryUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=38168999`;
+//         axios.get(queryUrl).then(function (res) {
+//             console.log(res.data);
+//         });
+//     });
+// }
+
+// function artist(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter a name of an artist you're looking for:"
+        }
+    ]).then(function (name) {
+            const queryUrl = `https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp`;
+            axios.get(queryUrl).then(function (res) {
+                console.log(res.data);
+            });
         });
-    });
-
-    
-
-
+    // }
 
 
    
